2c9f10e8db5181932e515e5f1c58fec4c625ecda
MariamKvirchiladze <kvircho555@gmail.com>
2024-02-16 01:38:01 +0400

    exercise The Locked Box - sollution

--- lesson-9.js
+++ lesson-9.js
@@ -87,27 +87,77 @@
 
 // Make sure you handle only the exceptions you are trying to handle.
 
-class MultiplicatorUnitFailure extends Error {}
+// class MultiplicatorUnitFailure extends Error {}
 
-function primitiveMultiply(a, b) {
-  if (Math.random() < 0.2) {
-    return a * b;
-  } else {
-    throw new MultiplicatorUnitFailure("Klunk");
-  }
-}
+// function primitiveMultiply(a, b) {
+//   if (Math.random() < 0.2) {
+//     return a * b;
+//   } else {
+//     throw new MultiplicatorUnitFailure("Klunk");
+//   }
+// }
 
-function reliableMultiply(a, b) {
+// function reliableMultiply(a, b) {
+//   // Your code here.
+//   while (true) {
+//     try {
+//       return primitiveMultiply(a, b);
+//     } catch (error) {
+//       if (!(error instanceof MultiplicatorUnitFailure)) {
+//         throw error; // rethrow if it's not the expected error
+//       }
+//     }
+//   }
+// }
+
+// console.log(reliableMultiply(8, 8));
+
+// The locked box
+// It is a box with a lock. There is an array in the box, but you can get at it only when the box is unlocked. Directly accessing the private _content property is forbidden.
+
+// Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, runs the function, and then ensures that the box is locked again before returning, regardless of whether the argument function returned normally or threw an exception.
+// For extra points, make sure that if you call withBoxUnlocked when the box is already unlocked, the box stays unlocked.
+
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
   // Your code here.
-  while (true) {
-    try {
-      return primitiveMultiply(a, b);
-    } catch (error) {
-      if (!(error instanceof MultiplicatorUnitFailure)) {
-        throw error; // rethrow if it's not the expected error
-      }
+  const wasLocked = box.locked;
+  if (wasLocked) {
+    box.unlock();
+  }
+  try {
+    body();
+  } finally {
+    if (wasLocked) {
+      box.lock();
     }
   }
 }
 
-console.log(reliableMultiply(8, 8));
+withBoxUnlocked(function () {
+  box.content.push("gold piece");
+  console.log(`gold is in the box`);
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
